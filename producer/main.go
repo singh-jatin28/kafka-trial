3f24fe72dcfd1fdfdeedbe790c71ac4f81dc483c
Jatin Singh <jatin.singh2@swiggy.in>
2022-08-09 09:29:23 +0530

    uploading all files for the assignment

---
+++ producer/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func main() {
+	w := &kafka.Writer{
+		Addr:                   kafka.TCP("localhost:9092"),
+		Topic:                  "example-123",
+		RequiredAcks:           kafka.RequireAll,
+		AllowAutoTopicCreation: true,
+		Async:                  true,
+		Completion: func(messages []kafka.Message, err error) {
+
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			for _, val := range messages {
+				fmt.Printf("messages sent, offset %d, key %s, val %s \n", val.Offset, val.Key, val.Value)
+			}
+		},
+	}
+
+	err := w.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: []byte("Hello World!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-B"),
+			Value: []byte("One!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-C"),
+			Value: []byte("Two!"),
+		},
+		kafka.Message{
+			Key:   []byte("Key-D"),
+			Value: []byte("Three!"),
+		},
+	)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
+
+	if err := w.Close(); err != nil {
+		log.Fatal("failed to close writer:", err)
+	}
+}
